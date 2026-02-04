4498341cb6787e4d100d9f4c0422b7bb8f89aa14
Patrick Augusto Góes de Aguiar Maia <patrick2maia@gmail.com>
2026-02-04 19:24:55 -0300

    deploy: mission gisa final orbit v12.0.0 - synced branding and technical core @spbr

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configuração técnica para garantir o funcionamento na Vercel
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist'
   }
 })
